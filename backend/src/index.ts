cec7eb4dd33db9b0fec5d99a909db46b3511be38
anik-saha-2001 <aniksahamda@gmail.com>
2024-05-31 20:38:19 +0530

    user register api completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
@@ -13,10 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // security prevents requests from certain urls
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express endpoint!" });
-});
-
+app.use("/api/users", userRoutes);
 app.listen(7000, () => {
   console.log(`Server is running on localhost:${7000}`);
 });
