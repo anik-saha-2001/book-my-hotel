8f892a90197ca5c08f5546c340ec386b4afc90f9
anik-saha-2001 <aniksahamda@gmail.com>
2024-06-02 23:26:04 +0530

    complete auth login api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(7000, () => {
   console.log(`Server is running on localhost:${7000}`);
 });
