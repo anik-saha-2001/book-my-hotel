a1c90f775a171789381a3d7341faf40f1871bdac
anik-saha-2001 <aniksahamda@gmail.com>
2024-07-26 18:26:30 +0530

    registration and token verification done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,23 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
 const app = express();
+app.use(cookieParser());
 // convert the body of api req to json
 app.use(express.json());
 // parse the url and get params like that
 app.use(express.urlencoded({ extended: true }));
 // security prevents requests from certain urls
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
