390111918e8706b1cb96afb878465ae8096acba4
anik-saha-2001 <aniksahamda@gmail.com>
2024-08-02 19:22:19 +0530

    added cross-env, .env.e2e file for e2e testing in e2e-db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,6 +24,7 @@ app.use(
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.listen(7000, () => {
-  console.log(`Server is running on localhost:${7000}`);
+const PORT = 7000;
+app.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
 });
