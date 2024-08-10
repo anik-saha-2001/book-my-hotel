63c3adba3c91a61ed4477302a0de03f5a6e5f13b
anik-saha-2001 <aniksahamda@gmail.com>
2024-08-10 11:33:14 +0530

    backend done for my hotels using cloudinary and multer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,17 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+// connect to cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
@@ -28,6 +37,8 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+
 const PORT = 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
