74e977b578bbe0304501c16e036b99ec144747e0
anik-saha-2001 <aniksahamda@gmail.com>
2024-08-19 19:45:13 +0530

    add backend for get all hotels

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -70,4 +70,13 @@ router.post(
   }
 );
 
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotel.find({ user: req.userId });
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error feching hotels" });
+  }
+});
+
 export default router;
